d82d37a6f57028e15efeb96909fa45eedd4c63d1
red08 <52722815+red08@users.noreply.github.com>
2019-07-09 22:30:18 -0700

    Create hi2.js

---
+++ hi2.js
@@ -0,0 +1,21 @@
+
+function Change_color() {
+  document.getElementById('title').style.color='#f5f56e';
+}
+
+function Change_back(){
+  document.getElementById('title').style.color='#7922e3';
+}
+
+function colorc(){
+  document.getElementById('header1').style.color='#f5f56e';
+  document.getElementById('header2').style.color='#f5f56e';
+  document.getElementById('header3').style.color='#f5f56e';
+  document.getElementById('header4').style.color='#f5f56e';
+}
+function colorb(){
+  document.getElementById('header1').style.color='#7922e3';
+  document.getElementById('header2').style.color='#7922e3';
+  document.getElementById('header3').style.color='#7922e3';
+  document.getElementById('header4').style.color='#7922e3';
+}
